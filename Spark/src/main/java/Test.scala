af03d5473a89c93af7cf35cdeb95f2e7bd55a0b9
xionghx <419330259@qq.com>
2023-02-03 20:17:21 +0800

    1

---
+++ Spark/src/main/java/Test.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @Author: Xionghx
+ * @Date: 2023/02/03/20:07
+ * @Version: 1.0
+ */
+object Test {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+  }
+}
