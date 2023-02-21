4724a514198b4056a23d37791333612668539c4a
xionghx <419330259@qq.com>
2023-02-21 09:37:37 +0800

    Elasticsearch笔记1

---
+++ Spark实时项目Spark Streaming/spark-realtime-1018/sparkStreaming-realtime/src/main/java/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Long a = 192168113115L & 2552552400L;
+        Long b = 192168113200L & 2552552400L;;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ Spark实时项目Spark Streaming/spark-realtime-1018/sparkStreaming-realtime/src/main/java/test1.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: Xionghx
+ * @Date: 2023/02/19/22:17
+ * @Version: 1.0
+ */
+public class test1 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.name = "as";
+        System.out.println(test.getName());
+    }
+}
